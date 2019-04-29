3ef31511447f83a62040493e82d9505b76149af7
Ana Paula Chaves <anapchavesweb@gmail.com>
2019-04-28 21:25:12 -0300

    Api de encontrar Pokemons por JSON
    work work work

---
+++ Pokedex/Pokedex/DescricaoPokemon.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pokedex
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DescricaoPokemon : ContentPage
+	{
+		public DescricaoPokemon (Pokemon poke)
+		{
+			InitializeComponent ();
+
+            imgPokemon.Source = new UriImageSource {
+                Uri = new Uri(poke.sprites.front_default),
+                CachingEnabled = true
+            };
+            
+            nomePokemon.Text = poke.name.ToUpper();
+            pesoPokemon.Text = poke.weight+"pounds";
+        
+        }
+	}
+}
--- Pokedex/Pokedex/MainPage.xaml.cs
+++ Pokedex/Pokedex/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +14,25 @@ namespace Pokedex
 		public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+            logoPokemon.Source = new UriImageSource { Uri = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/640px-International_Pok%C3%A9mon_logo.svg.png") };
+            
+        }
+
+        public async void PesquisarPokemon(object sender, EventArgs e) {
+
+            HttpClient client = new HttpClient();
+
+
+            var response = await client.GetAsync("https://pokeapi.co/api/v2/pokemon/" + txtPesquisaPokemon.Text);
+            if(response.IsSuccessStatusCode) {
+                var content = await response.Content.ReadAsStringAsync();
+                Pokemon resultado = JsonConvert.DeserializeObject<Pokemon>(content);
+                await Navigation.PushModalAsync(new DescricaoPokemon(resultado));
+                
+
+            }
+
+
+        }
+    }
 }
